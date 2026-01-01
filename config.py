6e8ccc6e18cd0278c8b3bbd6ec3709d8d803cde8
daggerTECH <53368091+daggerTECH@users.noreply.github.com>
2026-01-01 13:57:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    SECRET_KEY = os.environ.get("0b1bba2bef32b8faf0a404daf5378bd448ed04bf552642f91ef1231ff42b4687")
 
     # Gmail SMTP
     MAIL_SERVER = "smtp.gmail.com"
@@ -9,27 +9,12 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
 
-    EMAIL_USERNAME = "primeadsdigital@gmail.com"
-    EMAIL_PASSWORD = "mwwe grms mazj yqeg"
+    MAIL_USERNAME = "danny.villanueva@leaders.st"
+    MAIL_PASSWORD = "wsut sfvc ctja yofz"
 
     # SQLAlchemy Database
-    DATABASE_URL = os.getenv("mysql://root:yxpVkOzJCBXpybNYXFdmQXPvULefozpd@mysql.railway.internal:3306/railway")
-
-    if DATABASE_URL and DATABASE_URL.startswith("mysql://"):
-        DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
-
-    if not DATABASE_URL:
-        raise RuntimeError("❌ DATABASE_URL is not set")
-
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get("mysql://root:yxpVkOzJCBXpybNYXFdmQXPvULefozpd@nozomi.proxy.rlwy.net:52431/railway")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # SLACK NOTIFIER
     SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
-
-
-
-
-
-
-
