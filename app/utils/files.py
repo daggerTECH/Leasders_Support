a5a6f179fec9544df40d7c32f01626e8d5ab1c1f
daggerTECH <53368091+daggerTECH@users.noreply.github.com>
2026-01-02 21:53:32 +0800

    Update files.py

--- app/utils/files.py
+++ app/utils/files.py
@@ -5,7 +5,6 @@ def allowed_file(filename):
         "." in filename
         and filename.rsplit(".", 1)[1].lower()
         in current_app.config.get(
-            "ALLOWED_EXTENSIONS",
-            {"png", "jpg", "jpeg", "gif"}
+            "ALLOWED_EXTENSIONS", {"png", "jpg", "jpeg", "gif"}, "UPLOAD_FOLDER", {"static/"}
         )
     )
