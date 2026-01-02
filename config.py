ed4355d435ec0f7514932faca46f785fc5f2e6c6
daggerTECH <53368091+daggerTECH@users.noreply.github.com>
2026-01-02 18:57:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,9 @@ class Config:
     SLACK_WEBHOOK_URL = "SLACK_WEBHOOK_URL"
 
     # ATTACHMENTS
-    UPLOAD_FOLDER = "static/uploads"
+    UPLOAD_FOLDER = "uploads/tickets"
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB per file
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
 
 
