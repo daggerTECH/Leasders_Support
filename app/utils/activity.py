fce02e1602c31625b6e4dfe6592404f10fd691a4
daggerTECH <53368091+daggerTECH@users.noreply.github.com>
2026-01-09 21:09:06 +0800

    Create activity.py

---
+++ app/utils/activity.py
@@ -0,0 +1,16 @@
+from sqlalchemy import text
+from flask_login import current_user
+
+def log_ticket_activity(session, ticket_id, message):
+    session.execute(
+        text("""
+            INSERT INTO ticket_notes
+            (ticket_id, user_id, note, is_system)
+            VALUES (:ticket_id, :user_id, :note, 1)
+        """),
+        {
+            "ticket_id": ticket_id,
+            "user_id": current_user.id,
+            "note": message
+        }
+    )
